5c4376f185e3a9304f26bef620ac76d51258cb8a
robin-1412 <rajeshku1412@gmail.com>
2025-10-07 11:33:19 +0530

    Project Started

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+import sys
+
+import pygame
+    
+class AlienInvasion:
+
+    def __init__(self):
+
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+        #set background color
+        self.bg_color = (230,230,230)
+
+    def run_game(self):
+
+        while True:
+        # Watch for keyboard and mouse events.
+            for event in pygame.event.get():    
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+# Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
